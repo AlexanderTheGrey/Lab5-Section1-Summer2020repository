050ccfe450e759896df9be5e68095c2e85891877
AlexanderTheGrey <67391446+AlexanderTheGrey@users.noreply.github.com>
2020-07-13 23:52:27 -0500

    I updated the name file

--- FirstJava2.java
+++ FirstJava2.java
@@ -1,3 +1,4 @@
 System.out.println("My name is XXX and my hobby is YYY.");
 System.out.println("My name is AAA and my hobby is BBB.");
 System.out.println("My name is CCC and my hobby is DDD.");
+System.out.println("My name is DDD and my hobby is EEE.");
